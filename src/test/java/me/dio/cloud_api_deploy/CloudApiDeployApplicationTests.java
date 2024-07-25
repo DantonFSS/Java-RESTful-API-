ea7a25aef447b1f7d2607e84945f281fc5bebbbc
Danton <researches.dev.pro@gmail.com>
2024-07-25 13:36:05 -0700

    Test main

---
+++ src/test/java/me/dio/cloud_api_deploy/CloudApiDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.cloud_api_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
