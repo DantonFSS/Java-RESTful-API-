68883e63eb768917afdcffe48b00b3bf92db0d0f
Danton <researches.dev.pro@gmail.com>
2024-07-25 13:36:21 -0700

    Main application

---
+++ src/main/java/me/dio/cloud_api_deploy/CloudApiDeployApplication.java
@@ -0,0 +1,13 @@
+package me.dio.cloud_api_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiDeployApplication.class, args);
+	}
+
+}
